508d01c94090484dff3bc0915cd9c6000aaefd4c
egools <ergools59@gmail.com>
2020-01-28 00:26:07 -0500

    init

---
+++ BMGC_Fantasy_Parser/BMGC_Fantasy_Parser/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BMGC_Fantasy_Parser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
