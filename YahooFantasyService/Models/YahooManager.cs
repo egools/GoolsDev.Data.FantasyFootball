55670ec7524331cf84188c476734dec30f73b7db
egools <ergools59@gmail.com>
2021-01-29 01:10:12 -0500

    [GOOL-21] deserialize yahoo manager

--- YahooFantasyService/Models/YahooManager.cs
+++ YahooFantasyService/Models/YahooManager.cs
@@ -1,9 +1,18 @@
 ﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace YahooFantasyService
 {
     public class YahooManager
     {
+        [JsonConstructor]
+        public YahooManager(JToken manager)
+        {
+            ManagerId = manager["manager_id"].ToString();
+            Nickname = manager["nickname"].ToString();
+            Guid = manager["guid"].ToString();
+        }
+
         [JsonProperty(PropertyName = "manager_id")]
         public string ManagerId { get; set; }
 
