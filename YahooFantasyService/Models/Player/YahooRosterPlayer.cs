acedd637b96914cb420e40e5f36c734c1de5e916
egools <ergools59@gmail.com>
2021-02-26 03:56:57 -0500

    [GOOL-28] deserialize roster player

--- YahooFantasyService/Models/Player/YahooRosterPlayer.cs
+++ YahooFantasyService/Models/Player/YahooRosterPlayer.cs
@@ -1,10 +1,42 @@
-﻿namespace YahooFantasyService
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace YahooFantasyService
 {
     public class YahooRosterPlayer : YahooPlayerBase
     {
-        public string SelectedPosition { get; set; }
-        public string IsFlex { get; set; }
+        [JsonConstructor]
+        public YahooRosterPlayer(JToken selected_position)
+        {
+            var selectedPosition = new JObject();
+            selectedPosition.AbsorbTokenProperties(selected_position);
+            SelectedPosition = selectedPosition.ToObject<SelectedPosition>();
+        }
 
-        public string WeeklyPoints { get; set; }
+        public SelectedPosition SelectedPosition { get; set; }
+
+        public static YahooRosterPlayer FromJTokens(List<JToken> player)
+        {
+            var rosterPlayer = new JObject();
+            
+            rosterPlayer.AbsorbTokenProperties(player.First());
+            foreach(var j in player.Skip(1))
+            {
+                if (j.First is JProperty prop)
+                    rosterPlayer[prop.Name] = prop.Value;
+            }
+            return rosterPlayer.ToObject<YahooRosterPlayer>();
+        }
+    }
+
+    public class SelectedPosition
+    {
+        [JsonProperty("position")]
+        public string Position { get; set; }
+
+        [JsonProperty("is_flex")]
+        public bool IsFlex { get; set; }
     }
 }
