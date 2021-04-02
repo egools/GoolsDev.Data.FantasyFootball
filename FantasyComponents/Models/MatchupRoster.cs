f79131616211a743e8ada7f46c9e397a30e5469b
egools <ergools59@gmail.com>
2021-04-02 03:54:42 -0400

    [GOOL-71] update matchup roster to use yahoo based Ids

--- FantasyComponents/Models/MatchupRoster.cs
+++ FantasyComponents/Models/MatchupRoster.cs
@@ -1,22 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Diagnostics.CodeAnalysis;
-using System.Security.Policy;
 
 namespace FantasyComponents
 {
     [Table("MatchupRosters", Schema = "ff")]
     public class MatchupRoster
     {
-        public MatchupRoster()
+        public MatchupRoster(string rosterId, float score, float projected)
         {
+            RosterId = rosterId;
+            ActualScore = score;
+            ProjectedScore = projected;
             MatchupPlayers = new List<MatchupPlayer>();
         }
 
         [Key]
-        public string RosterId { get; set; } //[gameKey].l.[yahooLeagueId].t.[teamNum].[week]
+        public string RosterId { get; set; } //[gameKey].l.[yahooLeagueId].t.[teamNum].w.[week]
         public float ActualScore { get; set; }
         public float ProjectedScore { get; set; }
 
